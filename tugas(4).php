f61e3c5419cc865d2e8e9cb777774966d62e81e4
Pazrinuraliansyah <162782194+Pazrinuraliansyah@users.noreply.github.com>
2024-10-03 11:20:26 +0800

    Update tugas(4).php

--- tugas(4).php
+++ tugas(4).php
@@ -3,6 +3,9 @@ $ujian_teori= 80;
 $ujian_praktik = 80; 
 $nilai_lulus_teori= 70;
 $nilai_lulus_praktik= 75;
+if (($ujian_teori < 0 || $ujian_teori > 100) || ($ujian_praktik < 0 || $ujian_praktik > 100)) {
+    echo "nilai tidak valid.";
+} else 
 if ($ujian_teori>=$nilai_lulus_teori&&$ujian_praktik>=$nilai_lulus_praktik) {
     echo "selamat anda lulus";
 } else {
