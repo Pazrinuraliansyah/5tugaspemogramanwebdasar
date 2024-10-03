03b3b3fe92e112851f2db8b95a37d56e9d97b8f4
Pazrinuraliansyah <162782194+Pazrinuraliansyah@users.noreply.github.com>
2024-10-03 09:23:41 +0800

    Update tugas(2).php

--- tugas(2).php
+++ tugas(2).php
@@ -4,7 +4,7 @@ $harga_laptop=7000000;
 $harga_smartphone=3500000;
 $diskon_smartphone=0.10;
 $harga_smartphone_setelah_diskon=$harga_smartphone*(1-$diskon_smartphone);
-$total_pengeluaran=$harga_laptop+$harga_smartphone;
+$total_pengeluaran=$harga_laptop+$harga_smartphone_setelah_diskon;
 $sisa_tabungan=$tabungan-$total_pengeluaran;
 echo "sisa tabungan fajar adalah: Rp ". number_format($sisa_tabungan, 0,',','.');
 ?>
